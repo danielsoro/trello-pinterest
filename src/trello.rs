f630a7ca01ad4e0111a61c3bb593bbd7cb160e6a
Daniel Cunha <danielsoro@gmail.com>
2024-06-26 18:54:30 -0300

    feat(trello): added models to trello webhook

--- src/trello.rs
+++ src/trello.rs
@@ -1,21 +1,55 @@
-use axum::routing::get;
-use axum::{Json, Router};
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::post;
+use axum::{extract, Router};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
-pub struct TrelloRequest {}
+#[derive(Serialize, Deserialize)]
+pub struct ListChange {
+    pub id: String,
+    pub name: String,
+}
 
-#[derive(Serialize)]
-pub struct TrelloResponse {
-    value: String,
+#[derive(Serialize, Deserialize)]
+pub struct Board {
+    pub id: String,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Card {
+    pub id: String,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Data {
+    pub card: Card,
+    pub board: Board,
+    #[serde(rename = "listBefore")]
+    pub list_before: ListChange,
+    #[serde(rename = "listAfter")]
+    pub list_after: ListChange,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Action {
+    pub id: String,
+    pub data: Data,
+    #[serde(rename = "type")]
+    pub action_type: String,
+    pub date: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Root {
+    pub action: Action,
 }
 
 pub fn trello_routes_v1() -> Router {
-    Router::new().route("/trello/webhook", get(webhook_handler))
+    Router::new().route("/trello/webhook", post(webhook_handler))
 }
 
-async fn webhook_handler() -> Json<TrelloResponse> {
-    Json(TrelloResponse {
-        value: "Hello Trello Webhook".to_string(),
-    })
+async fn webhook_handler(extract::Json(_root): extract::Json<Root>) -> impl IntoResponse {
+    StatusCode::OK
 }
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,13 +1,42 @@
 use axum::http::StatusCode;
+use serde_json::json;
 use trello_pinterest::server::start_server;
+use trello_pinterest::trello::{Action, Board, Card, Data, ListChange, Root};
 
 #[tokio::test]
-async fn should_call_trello_webhook() {
+async fn webhook_should_be_able_to_get_card_move() {
     let _ = tokio::spawn(start_server());
 
     let hc = httpc_test::new_client("http://localhost:3000").unwrap();
     let response = hc
-        .do_get("/v1/trello/webhook")
+        .do_post(
+            "/v1/trello/webhook",
+            json!(Root {
+                action: Action {
+                    id: "667c8836c75b3e7efe887820".to_string(),
+                    data: Data {
+                        card: Card {
+                            id: "667c82ac89b9d50cb289994c".to_string(),
+                            name: "teste".to_string(),
+                        },
+                        board: Board {
+                            id: "667c8175ad220985c5a0d411".to_string(),
+                            name: "Development".to_string(),
+                        },
+                        list_before: ListChange {
+                            id: "667c8175ad220985c5a0d418".to_string(),
+                            name: "To Do".to_string()
+                        },
+                        list_after: ListChange {
+                            id: "667c8175ad220985c5a0d419".to_string(),
+                            name: "Doing".to_string()
+                        },
+                    },
+                    action_type: "updateCard".to_string(),
+                    date: "2024-06-26T21:27:22.722Z".to_string(),
+                }
+            }),
+        )
         .await
         .expect("The request to /v1/trello/webhook expected a response but it didn't reply");
     assert_eq!(StatusCode::OK.as_u16(), response.status().as_u16())
